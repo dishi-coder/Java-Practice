27d05b7278532b956adfad2b56b5e5419df850cc
Dishi Pawar <pawardishi3019@gmail.com>
2026-07-26 20:08:10 +0530

    Added while loop programs

---
+++ odd.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class odd {
+    public static void main(String[] args){
+        
+
+        for(int i=1;i<=100;i=i+2){
+            System.out.println(i);
+        }
+    }
+}
